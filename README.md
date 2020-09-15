c41be7e9becd23edb0926528290a4b7d7c163369
Annisa Purbandari <a.purbandari@gmail.com>
2020-09-15 17:59:10 +1000

    added render

--- README.md
+++ README.md
@@ -1,16 +1,41 @@
 # Day Planner
-
+*Day Planner* is a simple calendar application that helps user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 ## Goal 
+<p> AS AN employee with a busy schedule. I WANT to add important events to a daily planner. SO THAT I can manage my time effectively.</p>
+
+
 
 ## Contents
-
+Files Name |
+-----------|
+index.html |
+styles.css |
+script.js  |
 
 ## Acceptance Criteria 
-
+* GIVEN I am using a daily planner to create a schedule
+* WHEN I open the planner
+* THEN the current day is displayed at the top of the calendar
+* WHEN I scroll down
+* THEN I am presented with timeblocks for standard business hours
+* WHEN I view the timeblocks for that day
+* THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+* WHEN I click into a timeblock
+* THEN I can enter an event
+* WHEN I click the save button for that timeblock
+* THEN the text for that event is saved in local storage
+* WHEN I refresh the page
+* THEN the saved events persist
 
 ## Build With
+* [VScode] (http://code.visualstudio.com/) - The editor of choice.
+* [Chrome DevTools] (https://developers.google.com/web/tools/chrome-devtools) - The editor pages on-the-fly and problem diagnostic.
+* [W3C MarkUp Validator] (https://validator.w3.org/) - The markup validation service (HTML, XHTML) of web documents.
+* [GetBootstrap] (https://getbootstrap.com/) - Open-source CSS framework directed at responsive, mobile-first front-end web development. 
+
 
 ## Screenshot
+
 
 ### Link to the App
--- script.js
+++ script.js
@@ -16,27 +16,30 @@ function getCurrentDate() {
 }
 
 
-function createDatabase(currentDate) {
+function createDatabase() {
 
   //define data structures for day planner 
   //check if the stored data is today date if not empty data
   //create new element if empty in localstorage 
   getCurrentDate();
 
-  var myDay = {
-    hour: "10",
-    task: "sleep",
-  }
+  var myDay = [
+    {
+      hour: "17",
+      task: "sleep",
+    },
+    {
+      hour: "18",
+      task: "eat",
+    },
+    {
+      hour: "12",
+      task: "code",
+    }
+  ]
 
   var showDate = localStorage.setItem('myDay', JSON.stringify(myDay));
-  var retrievedObject = localStorage.getItem('myDay');
 
-  var getHourObject = JSON.parse(retrievedObject).hour;
-  var getTaskObject = JSON.parse(retrievedObject).task;
-
-
-  console.log("this is hour", getHourObject);
-  console.log("this is task", getTaskObject);
 
   if (showDate == null) {
 
@@ -50,6 +53,9 @@ function createDatabase(currentDate) {
 }
 
 
+
+
+
 // Loop through the data that was created above and render the data by showing it in the html
 function displayWorkPlanner() {
 
@@ -57,7 +63,52 @@ function displayWorkPlanner() {
   //create each html tag (p,textarea,button) and loop for each hour
   //make sure to make the add / save button work (create new function that render from data id)
 
+  var output = "";
 
+  var retrievedObject = localStorage.getItem('myDay');
+  console.log("retrieved", retrievedObject);
+
+  var schedule = JSON.parse(retrievedObject)
+
+
+  for (var i = 0; i < schedule.length; i++) {
+
+    var getHour = schedule[i].hour;
+    var getTask = schedule[i].task;
+
+    console.log(getHour);
+
+    //check if hour is past, present future
+    var momentPresent = moment().hour();
+    var momentCheck = getHour;
+
+    if (momentCheck < momentPresent) {
+      $('#task-area').css("background", "red");
+
+      console.log("past");
+
+    } else if (momentCheck = momentPresent) {
+      $('#task-area').css("background", "grey");
+      console.log("present");
+    } else {
+      $('#task-area').css("background", "blue");
+      console.log("future");
+    }
+
+    output = '<li class="row-agenda">' + getHour + '<textarea id="task-area">' + getTask + '</textarea>' + '<button id="' + getHour + '" class="save-button">' + '<i class="far fa-save fa-lg"></i>' + "add" + '</button>' + '</li>';
+    $(".container").append(output)
+
+
+  }
+
+  $(".save-button").click(
+    function () {
+      var id = this.id;
+      alert("checking button " + id);
+
+
+    }
+  );
 }
 
 
